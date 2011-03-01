831ee4becc55ee8d92f4b21d029e407c2243166d
bossrunner@gmail.com <bossrunner@gmail.com@dc3df0be-b572-6d72-2195-7d8e1d9750b7>
2011-03-01 15:40:27 +0000

    Added EDk project

---
+++ trunk/Reference/EDK_project/TestApp_PWM/TestApp_PWM.c
@@ -0,0 +1,120 @@
+/*
+ *
+ * Xilinx, Inc.
+ * XILINX IS PROVIDING THIS DESIGN, CODE, OR INFORMATION "AS IS" AS A 
+ * COURTESY TO YOU.  BY PROVIDING THIS DESIGN, CODE, OR INFORMATION AS
+ * ONE POSSIBLE   IMPLEMENTATION OF THIS FEATURE, APPLICATION OR 
+ * STANDARD, XILINX IS MAKING NO REPRESENTATION THAT THIS IMPLEMENTATION 
+ * IS FREE FROM ANY CLAIMS OF INFRINGEMENT, AND YOU ARE RESPONSIBLE 
+ * FOR OBTAINING ANY RIGHTS YOU MAY REQUIRE FOR YOUR IMPLEMENTATION
+ * XILINX EXPRESSLY DISCLAIMS ANY WARRANTY WHATSOEVER WITH RESPECT TO 
+ * THE ADEQUACY OF THE IMPLEMENTATION, INCLUDING BUT NOT LIMITED TO 
+ * ANY WARRANTIES OR REPRESENTATIONS THAT THIS IMPLEMENTATION IS FREE 
+ * FROM CLAIMS OF INFRINGEMENT, IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+ * Xilinx EDK 11.1 EDK_L.29.1
+ *
+ * This file is a sample test application
+ *
+ * This application is intended to test and/or illustrate some 
+ * functionality of your system.  The contents of this file may
+ * vary depending on the IP in your system and may use existing
+ * IP driver functions.  These drivers will be generated in your
+ * XPS project when you run the "Generate Libraries" menu item
+ * in XPS.
+ *
+ * Your XPS project directory is at:
+ *    C:\EDK11\Rebuild2\
+ */
+
+
+// Located in: ppc405_0/include/xparameters.h
+#include "xparameters.h"
+
+#include "xcache_l.h"
+
+#include "stdio.h"
+
+#include "xutil.h"
+
+#include "SystemTypes.h"
+
+#include "ServoControl.h"
+//====================================================
+
+int main (void) {
+	int8 str,thr;
+	str = 0;
+	thr = 8;  //this seems to keep the truck stopped.
+
+   XCache_EnableICache(0x00000001);
+   XCache_EnableDCache(0x00000001);
+   xil_printf("-- Starting PWM Test Mode --\r\n");
+	xil_printf("NOTICE: Make sure car is placed on stand before proceeding!!\r\n");
+	xil_printf("Use the number pad to control the car via Terminal\r\n");
+	xil_printf("8 Increment forward throttle\r\n");
+	xil_printf("4 Steer Left\r\n");
+	xil_printf("5 Center Servos\r\n");
+	xil_printf("6 Steer Right\r\n");
+	xil_printf("2 Increment reverse throttle\r\n");
+	xil_printf("x Center servos and exit\r\n\r\n");
+	
+	//Initialze servos to center position
+	ServoInit(RC_STR_SERVO);
+	ServoInit(RC_VEL_SERVO);
+	xil_printf("Steering Setting: %d\r\n",GetServo(RC_STR_SERVO));
+	xil_printf("Throttle Setting: %d\r\n\r\n",GetServo(RC_VEL_SERVO));
+
+	char c;
+	
+	while(1)
+	{
+	   read(0,&c,1);
+		switch (c)
+		{
+			case '4': //left
+				if(str < 64)
+					str += 8;
+				break;
+			case '5'://center
+				str = 0;
+				thr = 8;
+				break;
+			case '6'://right
+				if(str > -64)
+					str -= 8;
+				break;
+			case '8'://forward
+				if(thr < 64)
+					thr += 2;
+				break;
+			case '2'://reverse
+				if(thr > -64)
+					thr -= 2;
+				break;
+			case 'x'://quit
+				ServoInit(RC_STR_SERVO);
+				ServoInit(RC_VEL_SERVO);
+				print("-- Exiting main() --\r\n");
+				XCache_DisableDCache();
+				XCache_DisableICache();
+				return 0;
+				break;
+			default:
+				break;
+		}
+		SetServo(RC_STR_SERVO, str);
+		SetServo(RC_VEL_SERVO, thr);
+		xil_printf("Steering Setting: %d\r\n",GetServo(RC_STR_SERVO));
+		xil_printf("Throttle Setting: %d\r\n\r\n",GetServo(RC_VEL_SERVO));
+	}
+
+   print("-- Exiting main() --\r\n");
+   XCache_DisableDCache();
+   XCache_DisableICache();
+   return 0;
+}
+
---
+++ trunk/Reference/EDK_project/code/Common/HeliosIO.h
@@ -0,0 +1,95 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/******************************************************************************
+
+FILE:    HeliosIO.h
+AUTHOR:  Wade Fife
+CREATED: 11/07/05
+
+DESCRIPTION
+
+Header file for HeliosIO.c
+
+CHANGE LOG
+
+11/07/05 WSF  Created original file.
+12/02/05 WSF  Added code to control sleep pins on SRAM and flash.
+3/36/10 SFG Added game board IO functions
+
+******************************************************************************/
+
+#ifndef HELIOS_IO_H
+#define HELIOS_IO_H
+
+#include "Header.h"
+#include "xparameters.h"
+#include "xgpio.h"
+#include "xexception_l.h"
+
+
+// USER CONFIGURATION /////////////////////////////////////////////////////////
+
+// Baseaddress of GPIO module for board I/O
+#include "xparameters.h"
+#define HELIOS_IO_BASEADDR    XPAR_XPS_GPIO_0_BASEADDR
+
+// Define the components for which you would like to include code for
+// compilation.
+#define HELIOS_USE_BTN             // Code to read user button
+#define HELIOS_USE_SW1             // Code to read DIP SW1
+#define HELIOS_USE_SW2             // Code to read DIP SW2
+#define HELIOS_USE_LED1            // Code to read/write user LED1
+#define HELIOS_USE_LED2            // Code to read/write user LED2
+#undef HELIOS_USE_SLEEP_SRAM      // Code to sleep/awake SRAM
+#undef HELIOS_USE_SLEEP_FLASH     // Code to sleep/awake FLASH
+
+
+#define HELIOS_USE_GAME_SYSTEM    // Functions for using the game board GPIO interface
+	#define GPIO_DEVICE_ID				XPAR_XPS_GPIO_0_DEVICE_ID
+	#define INTC_DEVICE_ID				XPAR_XPS_INTC_0_DEVICE_ID
+	#define INTC_GPIO_INTERRUPT_ID	XPAR_XPS_INTC_0_XPS_GPIO_0_IP2INTC_IRPT_INTR
+	#define GAME_SYSTEM_GPIO_CHANNEL	2
+	#define GAME_SYSTEM_GPIO_INT		XPAR_XPS_GPIO_0_IP2INTC_IRPT_MASK
+	//bit definitions (for masks)
+	#define GAME_SHOOT_KILL				0x01
+	#define GAME_SHOOT_PASS				0x02
+	#define GAME_SHOOT_REVIVE			0x03
+	#define GAME_ENABLED_M				0x08
+	#define GAME_FLAG_M					0x10
+	#define GAME_HIT_M					0x20
+	#define GAME_PAUSE_M 				0x20
+	#define GAME_MISS_M					0x40
+	#define GAME_CAPTURED_M				0x40
+	#define GAME_WAIT_TO_SHOOT_M		0x80
+	
+///////////////////////////////////////////////////////////////////////////////
+
+
+
+// FUNCTION PROTOTYPES ////////////////////////////////////////////////////////
+#define Game_ClearShots() \
+	XGpio_DiscreteClear(&Gpio, GAME_SYSTEM_GPIO_CHANNEL, GAME_SHOOT_KILL|GAME_SHOOT_PASS|GAME_SHOOT_REVIVE);
+
+uint8  HeliosReadBTN(void);
+uint8  HeliosReadSW1(void);
+uint8  HeliosReadSW2(void);
+uint8  HeliosReadLED1(void);
+void   HeliosSetLED1(uint8 value);
+uint8  HeliosReadLED2(void);
+void   HeliosSetLED2(uint8 value);
+void   HeliosSetSleepSRAM(uint8 state);
+uint8  HeliosGetSleepSRAM(void);
+void   HeliosSetSleepFlash(uint8 state);
+uint8  HeliosGetSleepFlash(void);
+// Game board function prototypes
+void   Game_Shoot(uint8 shotType);
+uint8  Game_Enabled(void);
+uint8  Game_HaveFlag(void);
+void  Game_Hit(void);
+void  Game_Miss(void);
+uint8 Game_WaitingToShoot(void);
+void Game_Paused();
+void Game_UnPaused();
+void Game_FlagCaptured();
+XStatus InitGameSystem(void);
+
+#endif
