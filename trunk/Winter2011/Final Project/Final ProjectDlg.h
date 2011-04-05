b2d16190d8575704627fdf924cc930626adca87b
c.shirts@gmail.com <c.shirts@gmail.com@dc3df0be-b572-6d72-2195-7d8e1d9750b7>
2011-04-05 22:32:33 +0000

    committing sign language gui.

---
+++ trunk/Winter2011/Final Project/Final ProjectDlg.h
@@ -0,0 +1,42 @@
+// Final ProjectDlg.h : header file
+//
+
+#pragma once
+
+
+// CFinalProjectDlg dialog
+class CFinalProjectDlg : public CDialog
+{
+// Construction
+public:
+	CFinalProjectDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_FINALPROJECT_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+	BITMAPINFO  m_bitmapInfo;
+	CDC*	ImageDC;
+	CWnd*	ParentWnd;
+	RECT	ImageRect;
+	CRect	DispRect;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	void OnTimer( UINT nIDEvent );
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnSetPassword();
+	afx_msg void OnBnClickedSaveimg();
+	afx_msg void OnBnClickedStartentry();
+	afx_msg void OnBnClickedBtnSavetemplate();
+};
